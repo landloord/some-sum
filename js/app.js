aa7268b3d3347b32abb747efe6bade0ebbf37769
Landloord <agyabahopefaro@gmail.com>
2020-09-18 21:22:50 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,109 @@
+const resetBtn = document.querySelector(".reset");
+const submitBtn = document.querySelector(".submit");
+const initial = document.querySelector(".initial");
+const final = document.querySelector(".final");
+const gap = document.querySelector(".interval");
+const reverse = document.querySelector(".downwards");
+const checkBox = document.querySelector(".checkbox");
+const display = document.getElementsByClassName("results")[0];
+let sum = 0;
+let individual;
+let start;
+let interval;
+let num;
+let store = [];
+
+//
+// import logic from "./logic";
+import nav from "./nav";
+
+nav();
+// setValue();
+
+// function setValue() {
+final.addEventListener("change", (e) => {
+  if (e.target.value <= 0 || e.target.value == isNaN) {
+    e.target.value = 100;
+    num = parseFloat(e.target.value);
+  } else {
+    num = parseFloat(e.target.value);
+  }
+});
+initial.addEventListener("change", (e) => {
+  if (e.target.value <= 0 || e.target.value == isNaN) {
+    e.target.value = 0;
+    start = parseFloat(e.target.value);
+  } else {
+    start = parseFloat(e.target.value);
+  }
+});
+gap.addEventListener("change", (e) => {
+  if (e.target.value <= 0 || e.target.value == isNaN) {
+    e.target.value = 1;
+    interval = parseFloat(e.target.value);
+  } else {
+    interval = parseFloat(e.target.value);
+  }
+});
+checkBox.addEventListener("click", () => {
+  checkBox.children[0].checked = !checkBox.children[0].checked;
+});
+// }
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  function sumDown(i) {
+    if (i <= start) {
+      return "done";
+    } else {
+      sum += i;
+      store.push(sum);
+      return sumDown(i - interval);
+    }
+  }
+  var sumUp = (i) => {
+    if (start < i) {
+      start += interval;
+      sum += start;
+      store.push(sum);
+      return sumUp(i);
+    } else {
+      return "done";
+    }
+  };
+  if (!checkBox.children[0].checked) {
+    sumUp(num);
+    console.log(store);
+    store.forEach((item) => {
+      individual += `<p>${item}</p>`;
+    });
+    display.innerHTML = individual;
+    start = parseFloat(initial.value);
+    interval = parseFloat(gap.value);
+    num = parseFloat(final.value);
+    sum = 0;
+    store = [];
+  } else {
+    sumDown(num);
+    store.forEach((item) => {
+      individual += `<p>${item}</p>`;
+    });
+    display.innerHTML = individual;
+    start = parseFloat(initial.value);
+    interval = parseFloat(gap.value);
+    num = parseFloat(final.value);
+    sum = 0;
+    store = [];
+  }
+});
+
+resetBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  start = 0;
+  num = 0;
+  interval = 1;
+  display.innerHTML = "";
+  checkBox.children[0].checked = false;
+  store = [];
+});
---
+++ js/nav.js
@@ -0,0 +1,23 @@
+const navToggle = document.querySelector(".hamburger__container");
+const navLinks = document.querySelector(".nav__links");
+const links = document.querySelectorAll(".nav__links li a");
+const section = document.querySelector("section");
+
+const nav = () => {
+  navToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    navToggle.children[0].classList.toggle("open");
+    links.forEach((element) => {
+      element.addEventListener("click", () => {
+        navLinks.classList.remove("open");
+        navToggle.children[0].classList.remove("open");
+      });
+    });
+    section.addEventListener("click", () => {
+      navLinks.classList.remove("open");
+      navToggle.children[0].classList.remove("open");
+    });
+  });
+};
+
+export default nav;
